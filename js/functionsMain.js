8d96c3e3f3b16cf77128af42ed880a7a06ecfcd5
CamiloCMD <82065838+CamiloCMD@users.noreply.github.com>
2021-05-15 05:18:43 -0400

    COMMIT CAMILO 15/05/2021 5.20

--- js/functionsMain.js
+++ js/functionsMain.js
@@ -4,23 +4,22 @@ $(document).ready(function() {
   });
 
 function fn_indicadores(){
-    $.getJSON('https://api.libreapi.cl/economy/indicators', function(data){
+    $.getJSON('https://api.libreapi.cl/weather/stations', function(data){
         var info = data;
         
         $('#lbl_dolar').append('Dólar: $'+''+info.data.dolar);
         $('#lbl_euro').append('Euro: $'+info.data.euro);
         $('#lbl_uf').append('UF: $'+info.data.uf);    
     }).fail(function(){
-        alert('AUN NO CONECTA CON LA API, PAJARON');
+        alert('ERROR API INDICADORES');
     });
 }
 
 function fn_temperatura(){
     $.getJSON('https://api.libreapi.cl/weather/stations', function(data){
-        var info = data;
+      
         
-    
     }).fail(function(){
-        alert('AUN NO CONECTA CON LA API, PAJARON');
+        alert('ERROR API TEMPERATURA');
     });
 }
